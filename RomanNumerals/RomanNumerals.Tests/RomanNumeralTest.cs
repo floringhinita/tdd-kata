17b2e784f3da43f7dd497b0abbd4663e5794afad
Florin Ghinita <florin.ghinita@gmail.com>
2020-11-13 15:12:16 +0200

    M iteration

--- RomanNumerals/RomanNumerals.Tests/RomanNumeralTest.cs
+++ RomanNumerals/RomanNumerals.Tests/RomanNumeralTest.cs
@@ -3,59 +3,45 @@ using Xunit;
 namespace RomanNumerals.Tests
 {
     public class RomanNumeralTest
-    {
-        [Fact]
-        public void TestClassExists()
+    {        
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(10, "X")]
+        [InlineData(11, "XI")]
+        [InlineData(12, "XII")]
+        [InlineData(13, "XIII")]
+        [InlineData(14, "XIV")]
+        [InlineData(15, "XV")]
+        [InlineData(16, "XVI")]
+        [InlineData(17, "XVII")]
+        [InlineData(18, "XVIII")]
+        [InlineData(19, "XIX")]
+        [InlineData(20, "XX")]
+        [InlineData(30, "XXX")]
+        [InlineData(50, "L")]
+        [InlineData(60, "LX")]
+        [InlineData(85, "LXXXV")]
+        [InlineData(96, "XCVI")]
+        [InlineData(100, "C")]
+        [InlineData(127, "CXXVII")]
+        [InlineData(227, "CCXXVII")]
+        [InlineData(427, "CDXXVII")]
+        [InlineData(524, "DXXIV")]
+        [InlineData(943, "CMXLIII")]
+        [InlineData(1000, "M")]
+        [InlineData(1943, "MCMXLIII")]
+        public void Given_Input_Should_Return_Roman(int input, string expected)
         {
             var sut = new RomanNumerals();
 
-            Assert.IsType<RomanNumerals>(sut);
-        }
-
-        [Fact]
-        public void Given_Input_1_Should_Return_I()
-        {
-            var sut = new RomanNumerals();
-
-            var result = sut.Convert(1);
-            var expected = "I";
+            var result = sut.Convert(input);
 
             Assert.Equal(expected, result);
         }
-
-        [Fact]
-        public void Given_Input_2_Should_Return_II()
-        {
-            var sut = new RomanNumerals();
-
-            var result = sut.Convert(2);
-            var expected = "II";
-
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Given_Input_3_Should_Return_III()
-        {
-            var sut = new RomanNumerals();
-
-            var result = sut.Convert(3);
-            var expected = "III";
-
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Given_Input_4_Should_Return_IV()
-        {
-            var sut = new RomanNumerals();
-
-            var result = sut.Convert(4);
-            var expected = "IV";
-
-            Assert.Equal(expected, result);
-        }
-
 
     }
 }
--- RomanNumerals/RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -1,31 +1,42 @@
-﻿namespace RomanNumerals
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals
 {
     public class RomanNumerals
     {
+        private readonly Dictionary<int, string> dict = 
+            new Dictionary<int, string>
+            {
+                {1000, "M"},
+                {900, "CM"},
+                {500, "D"},
+                {400, "CD"},
+                {100, "C"},
+                {90, "XC"},
+                {50, "L"},
+                {40, "XL"},
+                {10, "X"},
+                {9, "IX"},
+                {5, "V"},
+                {4, "IV"},
+                {1, "I"},
+            };
+
         public string Convert(int number)
         {
-            if (number == 1)
+            var result = new StringBuilder();
+
+            foreach (var entry in dict)
             {
-                return "I";
+                while (number >= entry.Key)
+                {
+                    result.Append(entry.Value);
+                    number -= entry.Key;
+                }
             }
 
-            if (number == 2)
-            {
-                return "II";
-            }
-
-            if (number == 3)
-            {
-                return "III";
-            }
-
-            if (number == 4)
-            {
-                return "IV";
-            }
-
-
-            return "";
+            return result.ToString();
         }
     }
 }
